69c1bfff8389f40405e0fc665c0c73905f5e2b65
marauder77 <63757565+marauder77@users.noreply.github.com>
2021-09-28 21:28:49 -0700

    adding servers-test file

---
+++ servers.test.js
@@ -0,0 +1,45 @@
+describe("Servers test (with setup and tear-down)", function() {
+  beforeEach(function () {
+    // initialization logic
+    serverNameInput.value = 'Alice';
+  });
+
+  it('should add a new server to allServers on submitServerInfo()', function () {
+    submitServerInfo();
+
+    expect(Object.keys(allServers).length).toEqual(1);
+    expect(allServers['server' + serverId].serverName).toEqual('Alice');
+  });
+
+
+
+    it('should update server name', function () {
+      expect(updateServerTable).toEqual('key')//name?
+      expect(tipAverage).toEqual('$0.00')
+    }
+
+
+  })
+
+
+
+
+
+
+
+
+
+});
+
+
+
+//DON'T UNDERSTAND WHERE THIS COMES INTO PLAY OR WHY THESE ELEMNTS ARE BEING PICKED?
+// let curTdList = document.querySelectorAll('#serverTable tbody tr td');
+
+
+// expect(curTdList.length).toEqual(3);
+// expect(curTdList[0].innerText).toEqual('Alice');
+// expect(curTdList[1].innerText).toEqual('$0.00');
+// expect(curTdList[2].innerText).toEqual('X');
+
+
